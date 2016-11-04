6d56521d47fd297bfaea8d3bc587b4156d1cd865
navicester <navicester@qq.com>
2016-11-04 22:04:18 +0800

    db update

--- iexam/settings/base.py
+++ iexam/settings/base.py
@@ -87,12 +87,15 @@ WSGI_APPLICATION = 'iexam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+try:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		}
+	}
+except:
+	pass
 
 
 # Internationalization
